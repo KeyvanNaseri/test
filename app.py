4d20eedacef71b2224839d090cf1edc3e195bd42
Keyvan Naseri <keyvan.nasseri1375@gmail.com>
2024-07-14 09:03:26 +0000

    app.py added

---
+++ app.py
@@ -0,0 +1,6 @@
+def process_data(param1, param2):
+    result = param1 + param2
+    return result
+if __name__ == "__main__":
+    data1 = 10; data2 = 20
+    print(f"The result of processing data is: {process_data(data1, data2)}")
